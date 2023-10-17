d3712b9a41394a4820e305db956ca717fe41cb2f
ifatinha <ifatinha99@gmail.com>
2023-10-17 19:44:13 -0300

    tabnavigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-const tabmenu = document.querySelectorAll(".js-tabmenu a");
+const tabmenu = document.querySelectorAll(".js-tabmenu li");
+const tabcontent = document.querySelectorAll(".js-tabcontainer .instalacoes-item");
+
+(() => {
+
+    if (tabmenu.length && tabcontent.length) {
+        tabcontent[0].classList.add("is-active");
+
+        function openTab(index) {
+            tabcontent.forEach((content) => {
+                content.classList.remove("is-active");
+            })
+
+            tabcontent[index].classList.add("is-active");
+        }
+
+        tabmenu.forEach((link, index) => {
+            link.addEventListener('click', () => {
+                openTab(index);
+            })
+        })
+    }
+
+})();
